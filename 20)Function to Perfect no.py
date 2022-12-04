a7bcb282fd07679086ecf6a3b7479c08b6f6e882
radhaavaidya <60287291+radhaavaidya@users.noreply.github.com>
2022-12-04 21:56:40 +0530

    done

--- 20)Function to Perfect no.py
+++ 20)Function to Perfect no.py
@@ -4,6 +4,6 @@ def num_perfect(num):
     sum_n = 0  
     for i in range(1, num):  
         if num % i == 0:  
-            sum_n=sum_n+i  
+            sum_n=sum_n+i 
     return sum_n == num  
 print(num_perfect(26))  
