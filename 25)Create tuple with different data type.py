64089ff06877b0094f513aa96626d17e7ee62415
radhaavaidya <60287291+radhaavaidya@users.noreply.github.com>
2022-12-12 20:42:08 +0530

    done

---
+++ 25)Create tuple with different data type.py
@@ -0,0 +1,14 @@
+A=(2,5,6,8,1,9,2,71,2)
+print(A)
+#Float Tuple
+B=(2.2,22.5,22.6,88.6,22.01,33.05)
+print(B)
+#Character Tuple
+C=("A","B","C","D","E","F")
+print(C)
+#String Tuple
+D=("rbnb","cdj","dvp")
+print(D)
+#Mixed Tuple
+E=("rbnb",10,10.3,"10aaa","A")
+print(E)
